1f6cccfac0557be28e0274457bfed276c0206135
Leila <britezneira.leila@gmail.com>
2024-07-24 23:55:17 +0200

    Feat : add inputs variables

--- modal.js
+++ modal.js
@@ -12,6 +12,15 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Form inputs 
+const name = document.getElementById("name");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const location = document.querySelectorAll("checkbox-input[type=radio]");
+const checkboxInput = document.getElementById("checkbox1");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -19,5 +28,4 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 
